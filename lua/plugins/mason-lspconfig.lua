446ca342b32f526c4547186849740aa9888f6bdc
MartinDelgadoDev <gamadelgado@proton.me>
2025-10-21 21:49:16 -0500

    lsp change update

--- lua/plugins/mason-lspconfig.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = { "neovim/nvim-lspconfig", "hrsh7th/cmp-nvim-lsp" },
-  config = function()
-    require("config.lsp") -- aquí llamas a tu configuración personalizada
-  end,
-}
-
