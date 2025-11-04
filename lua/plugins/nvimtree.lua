244ea765c71b1644f684f2fb1d1b989d66e44d4a
MartinDelgadoDev <gamadelgado@proton.me>
2025-11-03 21:06:47 -0600

    toggle

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -14,6 +14,14 @@ return {
 						},
 					},
 				},
+                                actions = {
+                                open_file = {
+                                    quit_on_open = true,
+                                    }
+                                },
+                                filters = {
+                                    dotfiles=true,
+                                },
 			})
 		end,
 	},
